198719662e0f89bbe71f7f5c7550d094b4656c4f
Justin Yu <jusytinyu@gmail.com>
2019-07-17 22:49:13 -0400

    Even more makefile shenanigans

--- cpp/test/convertStr.cpp
+++ cpp/test/convertStr.cpp
@@ -10,7 +10,8 @@ int main(int argc, char const *argv[])
     cout << "scale" << std::to_string(ab->scale) << "\n";
     cout << "sscore:" << std::to_string(ab->sscore) << "\n";
     gamestate a;
-    tester::testAll(a,*ab);
+    array<int,9> *res = tester::testAll(a,*ab);
+    
     delete(ab);
     return 0;
 }
